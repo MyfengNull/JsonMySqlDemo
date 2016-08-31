21bbfcbfd791cc95452f802815f2ed79b94ea292
myfengnull <ysr200808@163.com>
2016-08-31 20:55:04 +0800

    push2

--- app/src/main/java/com/ysr/jsonmysqldemo/Main2Activity.java
+++ app/src/main/java/com/ysr/jsonmysqldemo/Main2Activity.java
@@ -1,9 +1,15 @@
 package com.ysr.jsonmysqldemo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ysr.jsonmysqldemo.utils.XcxjResult;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +19,8 @@ public class Main2Activity extends AppCompatActivity {
     @BindView(R.id.btnJexi)
     Button btnJexi;
     private TextView tv;
+    private Context context;
+    private LinearLayout linearLayoutdevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +28,36 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         ButterKnife.bind(this);
         tv = (TextView) findViewById(R.id.tvShow);
+        linearLayoutdevices = (LinearLayout) findViewById(R.id.lineLayoutdevices);
+        context = this;
     }
 
     @OnClick(R.id.btnJexi)
     public void onClick() {
-        
+        /**
+         * json方式
+         */
+        JSONObject obj = JSONObject.parseObject(XcxjResult.result);
+        JSONObject obj2 = obj.getJSONObject("body");
+        JSONArray obj3 = obj2.getJSONArray("deviceTask");
+        for (int i = 0; i < obj3.size(); i++) {
+            JSONObject obj4 = obj3.getJSONObject(i);
+            String tasknum = obj4.getString("tasknum");
+            TextView textView = new TextView(context);
+            textView.setText(tasknum);
+            linearLayoutdevices.addView(textView);
+        }
+
+/**
+ * gson方式
+ */
+
+//        XjxcBean newsbean = new Gson().fromJson(XcxjResult.result, XjxcBean.class);
+//        for (int i = 0; i <newsbean.body.deviceTask.size() ; i++) {
+//            String tasknum = newsbean.body.deviceTask.get(i).tasknum;
+//            TextView textView = new TextView(context);
+//            textView.setText(tasknum);
+//            linearLayoutdevices.addView(textView);
+//        }
     }
 }
